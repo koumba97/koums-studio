b158a64d1475ae71d95a78457a303f52825024e9
Koumba KEITA <koumba.keita@epitech.eu>
2025-11-10 09:54:36 -0500

    Extract control-container from Avatar to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,195 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import {
+    eyebrowsColor,
+    eyesColor,
+    hairColor,
+    lipsColor,
+    skinColor,
+} from './constants/ColorSettings';
+import './App.scss';
 import { Avatar } from './components/Avatar/Avatar';
+import {
+    CustomizationSettings,
+    settingList,
+    Option,
+    Settings,
+} from './constants/CustomizationSettings';
 
 function App() {
-    const [count, setCount] = useState(0);
+    const avatarDefaultSettings: Record<
+        Settings,
+        { color?: string; id: string }
+    > = {
+        face: {
+            color: skinColor[12],
+            id: 'face-1',
+        },
+        hair: {
+            color: hairColor[2],
+            id: 'hair-2',
+        },
 
+        eyes: {
+            color: eyesColor[2],
+            id: 'eyes-1',
+        },
+        eyebrows: {
+            color: eyebrowsColor[2],
+            id: 'eyebrows-1',
+        },
+        nose: {
+            id: 'nose-1',
+        },
+        lips: {
+            color: lipsColor[2],
+            id: 'lips-1',
+        },
+    };
+
+    const [currentSetting, setCurrentSetting] = useState<Settings>('face');
+    const [avatarUserSettings, setAvatarUserSettings] = useState(
+        avatarDefaultSettings
+    );
+
+    const handleColorChange = (setting: Settings, color: string) => {
+        const newAvatarSettings = { ...avatarUserSettings };
+        newAvatarSettings[setting].color = color;
+
+        setAvatarUserSettings(newAvatarSettings);
+    };
+
+    const handleShapeChange = (setting: Settings, shape: string) => {
+        const newAvatarSettings = { ...avatarUserSettings };
+        newAvatarSettings[setting].id = shape;
+
+        setAvatarUserSettings(newAvatarSettings);
+    };
     return (
         <>
-            <Avatar />
+            <Avatar avatarUserSettings={avatarUserSettings} />
+
+            <div className="control-container">
+                <div className="tabs-container">
+                    {settingList.map((setting, i) => {
+                        return (
+                            <button
+                                className={`setting-tab ${currentSetting === setting.id ? 'active' : ''}`}
+                                key={setting.id}
+                                data-text={setting.name}
+                                onClick={() => setCurrentSetting(setting.id)}
+                            >
+                                {setting.name}
+                            </button>
+                        );
+                    })}
+                </div>
+                <div className="setting-options">
+                    {CustomizationSettings.map((setting) => {
+                        if (setting.id === currentSetting) {
+                            return (
+                                <>
+                                    {setting.colorOptions ? (
+                                        <div className="colors-container">
+                                            <ColorOptionContainer
+                                                setting={setting.id}
+                                                currentColor={
+                                                    avatarUserSettings[
+                                                        setting.id
+                                                    ].color
+                                                }
+                                                colorList={setting.colorOptions}
+                                                onColorChange={
+                                                    handleColorChange
+                                                }
+                                            />
+                                        </div>
+                                    ) : null}
+                                    {
+                                        setting.attributes.length === 1 ? (
+                                            <div className="shapes-container">
+                                                {setting.attributes[0].options.map(
+                                                    (option, i) => (
+                                                        <ShapeOptionContainer
+                                                            currentShape={
+                                                                avatarUserSettings[
+                                                                    setting.id
+                                                                ].id
+                                                            }
+                                                            setting={setting.id}
+                                                            option={option}
+                                                            onShapeChange={
+                                                                handleShapeChange
+                                                            }
+                                                        />
+                                                    )
+                                                )}
+                                            </div>
+                                        ) : null // handle multiple attributes here
+                                    }
+                                </>
+                            );
+                        }
+                    })}
+                </div>
+            </div>
         </>
     );
 }
+
+type ColorOptionProp = {
+    setting: Settings;
+    colorList: string[];
+    currentColor?: string;
+    onColorChange: (setting: Settings, color: string) => void;
+};
+const ColorOptionContainer = ({
+    setting,
+    colorList,
+    currentColor,
+    onColorChange,
+}: ColorOptionProp) => {
+    return (
+        <>
+            {colorList.map((color, i) => {
+                return (
+                    <button
+                        className={`color-picker ${color === currentColor ? 'active' : null}`}
+                        onClick={() => onColorChange(setting, color)}
+                        style={{
+                            backgroundColor: color,
+                        }}
+                    />
+                );
+            })}
+
+            <input
+                type="color"
+                onChange={(e) => onColorChange(setting, e.target.value)}
+            />
+        </>
+    );
+};
+
+type ShapeOptionProp = {
+    setting: Settings;
+    option: Option;
+    currentShape?: string;
+    onShapeChange: (setting: Settings, shape: string) => void;
+};
+const ShapeOptionContainer = ({
+    setting,
+    option,
+    currentShape,
+    onShapeChange,
+}: ShapeOptionProp) => {
+    return (
+        <button
+            className={`shape-picker ${option.id === currentShape ? 'active' : null}`}
+            onClick={() => onShapeChange(setting, option.id)}
+        >
+            {option.id}
+        </button>
+    );
+};
 
 export default App;
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,6 +1,5 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import { useState } from 'react';
 import EyesAlmond from '../Face/EyesAlmond';
 import EyeBrows from '../Face/EyesBrows';
 import FaceShape from '../Face/FaceShape';
@@ -8,74 +7,15 @@ import Lips from '../Face/Lips';
 import Nose from '../Face/Nose';
 import Skin from '../Face/Skin';
 import './Avatar.scss';
-import {
-    CustomizationSettings,
-    Option,
-    settingList,
-    Settings,
-} from '../../constants/CustomizationSettings';
-import {
-    eyebrowsColor,
-    eyesColor,
-    hairColor,
-    lipsColor,
-    skinColor,
-} from '../../constants/ColorSettings';
+import { Settings } from '../../constants/CustomizationSettings';
 import HairBack from '../Face/HairBack';
 import HairFront from '../Face/HairFront';
 
-type CharacterProps = {
-    color?: string;
+type AvatarProps = {
+    avatarUserSettings: Record<Settings, { color?: string; id: string }>;
 };
 
-export const Avatar = ({ color = 'black' }: CharacterProps) => {
-    const avatarDefaultSettings: Record<
-        Settings,
-        { color?: string; id: string }
-    > = {
-        face: {
-            color: skinColor[12],
-            id: 'face-1',
-        },
-        hair: {
-            color: hairColor[2],
-            id: 'hair-2',
-        },
-
-        eyes: {
-            color: eyesColor[2],
-            id: 'eyes-1',
-        },
-        eyebrows: {
-            color: eyebrowsColor[2],
-            id: 'eyebrows-1',
-        },
-        nose: {
-            id: 'nose-1',
-        },
-        lips: {
-            color: lipsColor[2],
-            id: 'lips-1',
-        },
-    };
-    const [currentSetting, setCurrentSetting] = useState<Settings>('face');
-    const [avatarUserSettings, setAvatarUserSettings] = useState(
-        avatarDefaultSettings
-    );
-
-    const handleColorChange = (setting: Settings, color: string) => {
-        const newAvatarSettings = { ...avatarUserSettings };
-        newAvatarSettings[setting].color = color;
-
-        setAvatarUserSettings(newAvatarSettings);
-    };
-
-    const handleShapeChange = (setting: Settings, shape: string) => {
-        const newAvatarSettings = { ...avatarUserSettings };
-        newAvatarSettings[setting].id = shape;
-
-        setAvatarUserSettings(newAvatarSettings);
-    };
+export const Avatar = ({ avatarUserSettings }: AvatarProps) => {
     return (
         <>
             <div>
@@ -96,125 +36,6 @@ export const Avatar = ({ color = 'black' }: CharacterProps) => {
                     />
                 </div>
             </div>
-            <div className="settings-container">
-                {settingList.map((setting, i) => {
-                    return (
-                        <button
-                            className={`setting-tab ${currentSetting === setting.id ? 'active' : ''}`}
-                            key={setting.id}
-                            data-text={setting.name}
-                            onClick={() => setCurrentSetting(setting.id)}
-                        >
-                            {setting.name}
-                        </button>
-                    );
-                })}
-                <div className="setting-options">
-                    {avatarUserSettings.hair.id}
-                    {CustomizationSettings.map((setting) => {
-                        if (setting.id === currentSetting) {
-                            return (
-                                <>
-                                    {setting.colorOptions ? (
-                                        <div className="colors-container">
-                                            <ColorOptionContainer
-                                                setting={setting.id}
-                                                currentColor={
-                                                    avatarUserSettings[
-                                                        setting.id
-                                                    ].color
-                                                }
-                                                colorList={setting.colorOptions}
-                                                onColorChange={
-                                                    handleColorChange
-                                                }
-                                            />
-                                        </div>
-                                    ) : null}
-                                    {
-                                        setting.attributes.length === 1 ? (
-                                            <div className="shapes-container">
-                                                {setting.attributes[0].options.map(
-                                                    (option, i) => (
-                                                        <ShapeOptionContainer
-                                                            currentShape={
-                                                                avatarUserSettings[
-                                                                    setting.id
-                                                                ].id
-                                                            }
-                                                            setting={setting.id}
-                                                            option={option}
-                                                            onShapeChange={
-                                                                handleShapeChange
-                                                            }
-                                                        />
-                                                    )
-                                                )}
-                                            </div>
-                                        ) : null // handle multiple attributes here
-                                    }
-                                </>
-                            );
-                        }
-                    })}
-                </div>
-            </div>
         </>
-    );
-};
-
-type ColorOptionProp = {
-    setting: Settings;
-    colorList: string[];
-    currentColor?: string;
-    onColorChange: (setting: Settings, color: string) => void;
-};
-const ColorOptionContainer = ({
-    setting,
-    colorList,
-    currentColor,
-    onColorChange,
-}: ColorOptionProp) => {
-    return (
-        <>
-            {colorList.map((color, i) => {
-                return (
-                    <button
-                        className={`color-picker ${color === currentColor ? 'active' : null}`}
-                        onClick={() => onColorChange(setting, color)}
-                        style={{
-                            backgroundColor: color,
-                        }}
-                    />
-                );
-            })}
-
-            <input
-                type="color"
-                onChange={(e) => onColorChange(setting, e.target.value)}
-            />
-        </>
-    );
-};
-
-type ShapeOptionProp = {
-    setting: Settings;
-    option: Option;
-    currentShape?: string;
-    onShapeChange: (setting: Settings, shape: string) => void;
-};
-const ShapeOptionContainer = ({
-    setting,
-    option,
-    currentShape,
-    onShapeChange,
-}: ShapeOptionProp) => {
-    return (
-        <button
-            className={`shape-picker ${option.id === currentShape ? 'active' : null}`}
-            onClick={() => onShapeChange(setting, option.id)}
-        >
-            {option.id}
-        </button>
     );
 };
